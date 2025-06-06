27eeda185f6f88e7e103d9fe3b3ef9de5600807e
Yogndr <106022206+Yogndr@users.noreply.github.com>
2025-06-06 08:53:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,27 +3,26 @@
 
 // function Employee(name, age, jobTitle) {}
 
-class Person{
-	constructor(name,age){
-		this.name=name;
-		this.age=age;
-	}
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
-	greet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
-	}
+  greet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  }
 }
 
-class Employee extends Person{
+class Employee extends Person {
+  constructor(name, age, jobTitle) {
+    super(name, age);
+    this.jobTitle = jobTitle;
+  }
 
-	constructor(name,age,jobTitle){
-		super(name, age);
-		this.jobTitle=jobTitle;
-	}
-
-	jobGreet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
-	}
+  jobGreet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+  }
 }
 
 // Do not change code below this line
