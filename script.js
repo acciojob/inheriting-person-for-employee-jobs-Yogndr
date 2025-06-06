dcf4a0b5872498a54f507e96c877e086d353e16e
Yogndr <106022206+Yogndr@users.noreply.github.com>
2025-06-06 21:25:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
-// complete this js code
-// function Person(name, age) {}
 
-// function Employee(name, age, jobTitle) {}
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
+  greet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  }
 }
 
-Person.prototype.greet = function () {
-  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
-};
+class Employee extends Person {
+  constructor(name, age, jobTitle) {
+    super(name, age);
+    this.jobTitle = jobTitle;
+  }
 
-function Employee(name, age, jobTitle) {
-  Person.call(this, name, age); // Call the Person constructor
-  this.jobTitle = jobTitle;
+  jobGreet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+  }
 }
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
 
-// Inherit from Person
-Employee.prototype = Object.create(Person.prototype);
-Employee.prototype.constructor = Employee;
+// Person.prototype.greet = function () {
+//   console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+// };
 
-Employee.prototype.jobGreet = function () {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-};
+// function Employee(name, age, jobTitle) {
+//   Person.call(this, name, age); // Call the Person constructor
+//   this.jobTitle = jobTitle;
+// }
+
+// // Inherit from Person
+// Employee.prototype = Object.create(Person.prototype);
+// Employee.prototype.constructor = Employee;
+
+// Employee.prototype.jobGreet = function () {
+//   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+// };
 
 
 // Do not change code below this line
