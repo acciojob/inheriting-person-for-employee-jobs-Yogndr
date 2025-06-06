83f8e0bee49530b36f002491a866fd6576d8b389
Yogndr <106022206+Yogndr@users.noreply.github.com>
2025-06-06 21:18:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -25,6 +25,14 @@ Employee.prototype.jobGreet = function () {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
 
+const person = new Person("Alice", 25);
+person.greet(); 
+// Expected Output: Hello, my name is Alice, I am 25 years old.
+
+// Creating an instance of Employee
+const employee = new Employee("Bob", 30, "Manager");
+employee.jobGreet(); 
+
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
